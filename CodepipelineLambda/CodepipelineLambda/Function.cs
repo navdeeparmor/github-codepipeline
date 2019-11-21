fc1997a6df12d689db9ae912ca44f434974cc7a1
Navdeep <navdeep.veedu@armor.com>
2019-11-21 14:52:08 +0530

    checkin.

---
+++ CodepipelineLambda/CodepipelineLambda/Function.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using Newtonsoft.Json;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace CodepipelineLambda
+{
+    public class Function
+    {
+
+        /// <summary>
+        /// A simple function that takes a string and does a ToUpper
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public async Task<APIGatewayProxyResponse> FunctionHandler(APIGatewayProxyRequest apigProxyEvent, ILambdaContext context)
+        {
+            var body = new Dictionary<string, string>
+            {
+                { "message", "hello world" }
+            };
+
+            return new APIGatewayProxyResponse
+            {
+                Body = JsonConvert.SerializeObject(body),
+                StatusCode = 200,
+                Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
+            };
+        }
+    }
+}
