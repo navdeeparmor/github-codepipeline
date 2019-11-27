1331c810a8b77d853a518c296f7707398116232c
navdeep <navdeep.veedu@armor.com>
2019-11-27 21:33:17 +0530

    Test

--- CodepipelineLambda/CodepipelineLambda/Function.cs
+++ CodepipelineLambda/CodepipelineLambda/Function.cs
@@ -22,7 +22,7 @@ namespace CodepipelineLambda
         {
             var body = new Dictionary<string, string>
             {
-                { "message", "hello world here c" }
+                { "message", "hello world here co" }
             };
 
 
